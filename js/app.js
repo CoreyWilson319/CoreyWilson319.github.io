430c50b59d3c2247dd177912e0f0d536f5ad47f7
Your Name <you@example.com>
2020-11-10 11:15:30 -0500

    post presentation

--- js/app.js
+++ js/app.js
@@ -301,7 +301,6 @@ function lose() {
         ctx.textAlign = "center";
         ctx.color = 'blue'
         ctx.fillText("YOU LOSE!", 800, 100)
-        clearInterval(running)
     }
 }
 
@@ -324,8 +323,8 @@ document.getElementById("reset").addEventListener('click', function() {
     gameLive = true;
     hero.x = 20;
     hero.y = 300;
-    for (let i = 0; i < enemies.length; i++) {
-        enemies[i].x = randomNumber(800, 1400)
+    for (let i = 0; i < enemies; i++) {
+        enemies[i].x = randomNumber(500, 1400)
     }
     enemy.alive = true
     enemy1.alive = false
